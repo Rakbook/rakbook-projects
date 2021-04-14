8c723500ee78489f5888f9312f244ec7a9885c8d
LifeHasNoPurpose <mikolaj.juda@gmail.com>
2021-04-14 12:21:55 +0200

    added projects listing

---
+++ www/projects/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+	<title>Rakbook Community Projects</title>
+</head>
+<body>
+	<h1>Rakbook Community Projects</h1>
+	accessed at https://<i>name of the project</i>.rakbook.pl<br>
+	List of projects:<br>
+	<ul>
+	<?php
+		chdir('..');
+		$projects = array_diff(glob('*' , GLOB_ONLYDIR), ['projects']);
+		foreach($projects as &$p)
+		{
+			echo '<li><a href="https://'.$p.'.projects.rakbook.pl">'.$p.'</a></li>';
+		}
+	?>
+	</ul>
+</body>
